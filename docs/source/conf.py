4cca76eb8d384560605e9fd7fadd4e59119ad8eb
hakalasonja <123334596+hakalasonja@users.noreply.github.com>
2023-11-13 18:00:33 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'tempo'
+copyright = '2023, Sonja Hakala'
+author = 'Sonja Hakala'
 
 release = '0.1'
 version = '0.1.0'
