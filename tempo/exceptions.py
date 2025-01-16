9ec1e22bc4a5575bcd87673082ed58d5d4c21660
oonjnertzern <oonjnertzern@gmail.com>
2025-01-16 17:31:20 -0500

    Update exceptions.py

--- tempo/exceptions.py
+++ tempo/exceptions.py
@@ -2,28 +2,22 @@
 # -*- coding: utf-8 -*-
 
 """
-Created on Tue Nov 19 
-
 Utility file meant to encapsulate custom exception classes. These custom exceptions
-are used in testing, and ensuring that the user gets feedback on common errors to 
+are used in testing to ensure that the user gets feedback on common errors to 
 differentiate between a library issue and a user input issue.
 
-If you find one of these exceptions have been thrown in your project, please
-refer to the docs for the class you are trying to use to make sure everything is right.
-ALSO ensure that when using iterables you strictly use supported types. I.e. checks
-are only done for whether or not you provide iterables, however many type hints
-include Iterables of - say - type Pulse. These iterables cannot be strings, therefore,
-they must be lists of Pulse objects. Please ensure that you correctly use lists then. 
+If you find one of these exceptions in your project, please
+refer to the documentation for troubleshooting.
+When using iterables, please ensure that you strictly use the supported types, as checks
+are only done for whether or not you provide iterables. 
 
-I.e. while Iterable support is the only thing that is mandatory in most cases, this does not 
-mean that the use of an iterable object will guarantee the code works. Using lists is the wisest
-approach to guaranteeting the success of the provided code. 
+I.e. while Iterable support is mandatory in most cases, 
+this does not mean that simply using an iterable object will guarantee that the code works. 
+Using lists is the safest approach to guaranteeting the success of the code. 
 
 If you do NOT receive one of these exceptions, but something is going wrong, please
-open an issue on github --> Unless the issue occurs within your own custom provided functions!
+open an issue on Github --> Unless the issue occurs within your own custom provided functions!
 
-Essentially, these exception definitions will catch if there is something 
-obvious wrong with the user's input. 
 
 @author: georgew79
 """
