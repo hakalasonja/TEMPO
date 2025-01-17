aa15a176ddb4db6659761c6524eaed07e6419f1b
oonjnertzern <oonjnertzern@gmail.com>
2025-01-16 19:44:37 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,7 +34,9 @@ templates_path = ['_templates']
 
 # -- Options for HTML output
 
-html_theme = 'sphinx_rtd_theme'
+#html_theme = 'sphinx_rtd_theme'
+html_style = 'css/yourtheme.css'
+
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
